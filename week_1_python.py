c21edc858ca431a5f7199e7e53dbac3d636c3652
naomitesfe <tesfayenuhamin@gmail.com>
2025-08-04 18:46:56 +0300

    added files

---
+++ week_1_python.py
@@ -0,0 +1,13 @@
+x = float(input("please input x"))
+y = float(input("Please input y"))
+z = input("please input operation")
+if z == "+":
+   print("x + y = ", x+y);
+elif z == "-":
+   print("x - y = ",x-y);
+elif z=="/":
+   print("x / y = ", x/y)
+elif z=="*":
+   print("x * y = ", x*y)
+else:
+   print("please choose from +,-,/,*");
