c887bf2d2efc10520c7d24e683bb19ae3cbc0cc9
naomitesfe <87719049+naomitesfe@users.noreply.github.com>
2025-08-28 10:40:59 +0300

    Create polymorphism.py

---
+++ week-5/polymorphism.py
@@ -0,0 +1,21 @@
+class Vehicle:
+    def move(self):
+        raise NotImplementedError("Subclass must implement this method")
+
+class Car(Vehicle):
+    def move(self):
+        return "🚗 Driving on the road!"
+
+class Plane(Vehicle):
+    def move(self):
+        return "✈️ Flying in the sky!"
+
+class Boat(Vehicle):
+    def move(self):
+        return "⛵ Sailing on the water!"
+
+# Test polymorphism
+vehicles = [Car(), Plane(), Boat()]
+
+for v in vehicles:
+    print(v.move())
